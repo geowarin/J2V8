2529e2797bc5c0933bf98d2b21c9af999866eba9
geowarin <geowarin@gmail.com>
2017-02-19 18:12:43 +0100

    Fix maven artifact id

--- build-scripts/linux/build.sh
+++ build-scripts/linux/build.sh
@@ -1,32 +1,33 @@
 #!/usr/bin/env bash
 
 JDK7="/usr/lib/jvm/java-7-openjdk-amd64"
-#export PYTHON=/usr/bin/python
+NODE=./node
+#NODE=/home/vagrant/node
 
 mkdir -p ./build/linux
 
 g++ \
--I./node \
--I./node/deps/v8 \
--I./node/deps/v8/include \
--I./node/src \
+-I${NODE} \
+-I${NODE}/deps/v8 \
+-I${NODE}/deps/v8/include \
+-I${NODE}/src \
 -I ${JDK7}/include/ \
 -I ${JDK7}/include/linux  \
 ./jni/com_eclipsesource_v8_V8Impl.cpp -std=c++11 -fPIC -shared \
 -o ./build/linux/libj2v8_linux_x86_64.so \
 -Wl,--whole-archive \
-./node/out/Release/libnode.a \
+${NODE}/out/Release/libnode.a \
 -Wl,--no-whole-archive \
 -Wl,--start-group \
-  ./node/out/Release/libv8_libbase.a \
-  ./node/out/Release/libv8_libplatform.a \
-  ./node/out/Release/libv8_base.a \
-  ./node/out/Release/libv8_nosnapshot.a \
-  ./node/out/Release/libuv.a \
-  ./node/out/Release/libopenssl.a \
-  ./node/out/Release/libhttp_parser.a \
-  ./node/out/Release/libgtest.a \
-  ./node/out/Release/libzlib.a \
-  ./node/out/Release/libcares.a \
+  ${NODE}/out/Release/libv8_libbase.a \
+  ${NODE}/out/Release/libv8_libplatform.a \
+  ${NODE}/out/Release/libv8_base.a \
+  ${NODE}/out/Release/libv8_nosnapshot.a \
+  ${NODE}/out/Release/libuv.a \
+  ${NODE}/out/Release/libopenssl.a \
+  ${NODE}/out/Release/libhttp_parser.a \
+  ${NODE}/out/Release/libgtest.a \
+  ${NODE}/out/Release/libzlib.a \
+  ${NODE}/out/Release/libcares.a \
 -Wl,--end-group \
 -lrt -D NODE_COMPATIBLE=1
--- linux-build-with-docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker rm -v j2v8-linux
-docker build --rm=true --no-cache -t "eclipsesource/j2v8-linux-build" ./build-scripts/linux/
-docker run --name j2v8-linux eclipsesource/j2v8-linux-build
-#docker cp j2v8-linux:/data/v8_3_26/out .
